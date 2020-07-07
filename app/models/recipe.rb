0488bed7eeface27a0ab59b42a38b1c8f7694fd8
vagrant <vagrant@localhost.localdomain>
2020-07-07 13:20:49 +0000

    Add modelの紐付け

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,17 @@
 class Recipe < ApplicationRecord
+
+	attachment :image
+
+	belongs_to :user
+	belongs to :genre
+	belongs_to :type
+	has_many :comments
+	has_many :likes
+	has_many :recipe_tag
+
+	# recipeにいいねしていたらtureを返す
+	def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+    end
+
 end
