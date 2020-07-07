a0b2c65927c834b824ee20a7f6301df0b9989d8e
vagrant <vagrant@localhost.localdomain>
2020-07-07 12:54:59 +0000

    Add userコントローラ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
 	before_action :authenticate_user!
+	before_action :set_user
 
 	def show
+		@recipes = @user.recipes
 	end
 
 	def edit
@@ -11,11 +13,22 @@ class UsersController < ApplicationController
 	end
 
 	def unsubscribe
+		if @user != current_user
+			flash[:alert] = "不正なアクセスです"
+			redirect_to root_path
+		end
 	end
 
 	def leave
 	end
-	
 
+private
+	def user_params
+		params.require(:user).permit(:name, :email, :image, :introduction)
+	end
+
+	def set_user
+		@user = User.find(params[:id])
+	end
 
 end
