5c46c8ea9706ff72eceae95f472bbfc056d2def3
vagrant <vagrant@localhost.localdomain>
2020-07-09 17:38:51 +0000

    recipeテーブルにuser_idを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_111209) do
+ActiveRecord::Schema.define(version: 2020_07_09_173738) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2020_07_07_111209) do
     t.text "make"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "relationships", force: :cascade do |t|
