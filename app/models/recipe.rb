f83755dbdaba1ea934c6021cee009cc0fd50f4eb
vagrant <vagrant@localhost.localdomain>
2020-07-13 03:09:11 +0000

    model修正

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,10 +2,13 @@ class Recipe < ApplicationRecord
 
 	attachment :image
 
+	belongs_to :genre
+	belongs_to :type
 	belongs_to :user
 	has_many :comments
 	has_many :likes
-	has_many :recipe_tag
+	has_many :recipe_tags
+
 
 	# recipeにいいねしていたらtureを返す
 	def liked_by?(user)
