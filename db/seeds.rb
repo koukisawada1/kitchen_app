0aa59b0f2cea602b24d79d294a93e373506b271f
vagrant <vagrant@localhost.localdomain>
2020-07-13 04:03:54 +0000

    画像の追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ Genre.create!(
       name: "その他"
     }
   ]
-)
+  )
 
 Type.create!(
   [
@@ -42,11 +42,61 @@ Type.create!(
       name: "その他"
     }
   ]
-)
+  )
 
 User.create!(
-      email: "test@test.com",
-      name: "test",
+  [
+    {
+      email: "maikeru@maikeru.com",
+      name: "マイケル",
+      introduction: "料理するのが大好きです",
       password: "password",
-      password_confirmation: "password",
-      )
+      password_confirmation: "password"
+    },
+    {
+      email: "ro-ranndo@ro-ranndo.com",
+      name: "ローランド",
+      introduction: "最近料理始めました",
+      password: "password",
+      password_confirmation: "password"
+    },
+    {
+      email: "rea-do@rea-do.com",
+      name: "レアード",
+      introduction: "料理初心者ですがよろしくお願いします",
+      password: "password",
+      password_confirmation: "password"
+    },
+    {
+      email: "sugoya@sugiya.com",
+      name: "杉谷",
+      introduction: "元料理人です。よろしくお願いします",
+      password: "password",
+      password_confirmation: "password"
+    },
+    {
+      email: "darubissyu@darubissyu.com",
+      name: "ダルビッシュ",
+      introduction: "元料理人です。よろしくお願いします",
+      password: "password",
+      password_confirmation: "password"
+    }
+  ]
+  )
+for i in 1..15 do
+  Recipe.create!(
+    [
+      {
+        user_id: rand(1..5),
+        title: "レシピタイトル#{i}",
+        body: "料理紹介文",
+        price: rand(2..5) * 100,
+        puantity: rand(1..4),
+        material: "材料名#{i}",
+        make: "作り方",
+        genre_id: rand(1..5),
+        tyep_id: rand(1..7)
+      }
+    ]
+    )
+
