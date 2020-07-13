61946c4c151e036ee25141683f63fd8f44182b72
vagrant <vagrant@localhost.localdomain>
2020-07-13 07:46:19 +0000

    レシピ一覧ページ作成

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,27 +1,73 @@
 class RecipesController < ApplicationController
 
 	def index
+		@genres = Genre.all
+		@types = Type.all
+		@tags = Tag.all
+		if params[:genre_id]
+			@genre = Genre.find(params[:genre_id])
+			@recipes = @genre.recipes
+			@title = @genre.name
+		elsif params[:type_id]
+			@type = Type.find(params[:type_id])
+			@recipes = @type.recipes
+			@title = @type.name
+		else
+			@recipes = Recipe.all
+			@title = "全てのレシピ"
+		end
 	end
 
 	def show
+		@recipe = Recipe.find(params[:id])
+		@comment = Comment.new
+		@comments = @recipe.comments
 	end
 
 	def new
+		recipe = Recipe.new
 	end
 
+# 確認画面
 	def confirm
 	end
 
 	def create
+		recipe = Recipe.new(recipe_params)
+		if recipe.save
+			flash[:notice] = "レシピを投稿しました"
+			redirect_to recipe_path(recipe.id)
+		else
+			flash[:alert] = "入力内容を確認したください"
+			render "new"
+		end
 	end
 
 	def edit
+		@recipe = Recipe.find(params[:id])
 	end
 
 	def update
+		@recipe = Recipe.find(params[:id])
+		if @recipe.update(recipe_params)
+			flash[:notice] = "レシピを更新しました"
+			redirect_to recipe_path(@recipe.id)
+		else
+			flash[:alert] = "入力内容を確認してください"
+			render "edit"
+		end
 	end
 
 	def destroy
+		@recipe = Recipe.find(params[:id])
+		@recipe.destroy
+		redirect_to user_path(current_user.id)
+	end
+
+	private
+
+	def recipe_params
+		params.require(:recipe).permit(:title, :image, :body, :price, :quantity, :material, :make)
 	end
 
 end
