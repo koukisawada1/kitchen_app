8f5afd4e18030a4488d92724f1bd8e874b0776e4
vagrant <vagrant@localhost.localdomain>
2020-07-14 13:11:57 +0000

    いいね機能追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,9 +5,18 @@ class LikesController < ApplicationController
 	end
 
 	def create
+		@recipe = Recipe.find(params[:recipe_id])
+		@like = @recipe.likes.new(user_id: current_user.id)
+		@like.save
+		redirect_to recipe_path(@recipe)
 	end
 
 	def destroy
+		@recipe = Recipe.find(params[:recipe_id])
+		@like = current_user.likes.find_by(recipe_id: @recipe.id)
+		@like.destroy
+		redirect_to recipe_path(@recipe)
 	end
+
 
 end
