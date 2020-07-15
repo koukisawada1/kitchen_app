37ea9d36f7cc305907ee88cb3391a18d3be03332
vagrant <vagrant@localhost.localdomain>
2020-07-15 06:00:58 +0000

    topページ機能実装

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,10 @@
 class HomesController < ApplicationController
 
 	def top
+		@genres = Genre.all
+		@types = Type.all
+		@like_recipes = Recipe.find(Like.group(:recipe_id).order('count(recipe_id) desc').limit(2).pluck(:recipe_id))
+		@recipes = Recipe.all.order(created_at: :desc).limit(2) #=>新しい順の投稿2件
 	end
 
 	def about
