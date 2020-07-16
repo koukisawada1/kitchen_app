eee3120a9576e33fa359e26f3f59671d6a2f73cb
vagrant <vagrant@localhost.localdomain>
2020-07-16 07:40:59 +0000

    画像プレビュー機能追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,21 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
 //= require activestorage
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+$(function(){
+    // inputのidから情報の取得
+    $('#product-image').on('change', function (e) {
+// ここから既存の画像のurlの取得
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        $(".image").attr('src', e.target.result);
+    }
+// ここまで
+    reader.readAsDataURL(e.target.files[0]); //取得したurlにアップロード画像のurlを挿入
+});
+});
