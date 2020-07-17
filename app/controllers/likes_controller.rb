5ed63826bdc46b3b8c334499834d2121fd8e7128
vagrant <vagrant@localhost.localdomain>
2020-07-17 10:28:18 +0000

    いいね機能非同期化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,15 +4,13 @@ class LikesController < ApplicationController
 		@recipe = Recipe.find(params[:recipe_id])
 		@like = @recipe.likes.new(user_id: current_user.id)
 		@like.save
-		redirect_to recipe_path(@recipe)
+		# redirect_to recipe_path(@recipe)
 	end
 
 	def destroy
-		recipe = Recipe.find(params[:recipe_id])
-		like = current_user.likes.find_by(recipe_id: recipe.id)
-		like.destroy
-		redirect_to recipe_path(recipe)
+		@recipe = Recipe.find(params[:recipe_id])
+		@like = current_user.likes.find_by(recipe_id: @recipe.id)
+		@like.destroy
+		# redirect_to recipe_path(recipe)
 	end
-
-
 end
