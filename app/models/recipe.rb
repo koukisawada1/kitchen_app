b1fb82c60a93e753cfcd2c3c29274c2429517d37
vagrant <vagrant@localhost.localdomain>
2020-07-17 07:12:06 +0000

    レシピモデルバリテーション追加

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -9,6 +9,15 @@ class Recipe < ApplicationRecord
 	has_many :likes
 	has_many :recipe_tags
 
+	# バリテーション
+	validates :title, presence: true, length: { maximum: 30 }
+	validates :body, presence: true, length: { maximum: 200 }
+	validates :price, presence: true
+	validates :quantity, presence: true
+	validates :material, presence: true
+	validates :make, presence: true
+	validates :genre_id, presence: true
+	validates :type_id, presence: true
 
 	# recipeにいいねしていたらtureを返す
 	def liked_by?(user)
